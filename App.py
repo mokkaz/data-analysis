5f0331ae9ba1b7a64b776509e86116fbaef16878
mokkaz <karim.mokbel01@gmail.com>
2024-05-04 20:12:22 +0300

    Create App.py
    Initialized App

---
+++ App.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+st.set_page_config(page_title= 'Dashboard', page_icon=":bar_chart:",layout="wide")
