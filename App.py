5135d08b424caa38f4705c6a8a1d53c884dd78ec
mokkaz <karim.mokbel01@gmail.com>
2024-05-04 20:43:15 +0300

    Created Navigation sidebar

--- App.py
+++ App.py
@@ -13,3 +13,39 @@ def datafile_uploader():
     return df
     
 df = datafile_uploader()
+
+#Sidebar navigation
+st.sidebar.title('Navigation')
+options = st.sidebar.radio('Select what you want to display:', 
+                           ['Home', 'Data Summary', 'Data Header', 'Scatter Plots', 'Correlations', 'Biodiversity Calculator', 'Metrics Statistics'])
+
+def home():
+    st.write('home')
+def data_summary():
+    st.write('data_summary')
+def data_header():
+    st.write('data_header')
+def scatter_plot():
+    st.write('scatter_plot')
+def correlation_plot():
+    st.write('correlation_plot')
+def calc_biodiversity():
+    st.write('calc_biodiversity')
+def metrics_stats():
+    st.write('metrics_stats')
+
+# Navigation options
+if options == 'Home':
+    home()
+elif options == 'Data Summary':
+    data_summary()
+elif options == 'Data Header':
+    data_header()
+elif options == 'Scatter Plots':
+    scatter_plot()
+elif options == 'Correlations':
+    correlation_plot()  
+elif options == 'Biodiversity Calculator':
+    calc_biodiversity()  
+elif options == 'Metrics Statistics':
+    metrics_stats()  
