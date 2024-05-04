fa881c86e5e6eb8bf74504b6972e6f88aa31b07c
mokkaz <karim.mokbel01@gmail.com>
2024-05-04 20:22:42 +0300

    Uploaded 4Lences.csv data file to the app

--- App.py
+++ App.py
@@ -4,3 +4,12 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 
 st.set_page_config(page_title= 'Dashboard', page_icon=":bar_chart:",layout="wide")
+
+# Add a title and intro text
+st.title('Biodiversity Metrics Explorer')
+
+def datafile_uploader():
+    df = pd.read_csv('4Lenses.csv')
+    return df
+    
+df = datafile_uploader()
