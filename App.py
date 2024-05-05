4af7d033ba195416dd8bda31d49a30b9990df80f
mokkaz <karim.mokbel01@gmail.com>
2024-05-05 14:40:16 +0300

    Added a requirements.txt file

--- App.py
+++ App.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
-from pandas.api.types import is_numeric_dtype
 
 st.set_page_config(page_title= 'Dashboard', page_icon=":bar_chart:",layout="wide")
 
